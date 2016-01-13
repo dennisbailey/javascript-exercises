9b74f2117d1b2b73350d0f7314cb493f65054da7
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-13 08:51:43 -0700

    Wes's arguments function challenge

---
+++ wes-function.js
@@ -0,0 +1,28 @@
+function argTest (arg1, arg2, arg3) {
+    if (arguments.length === 0) {console.log("No arguments!")}
+    else if (arguments.length > 3) {console.log("AARG! Too many arguments!")}
+    else{
+        console.log("Reading arguments…");
+        var args = Array.prototype.slice.call(arguments);
+        console.log(args);
+        for (i = 0; i < args.length; i++) {
+           if (i !== undefined) {console.log("Reading argument #" + (i+1) + ": is", args[i]);}
+        }
+    }
+}
+
+
+console.log("TESTING: ALL UNDEFINED");
+argTest();
+console.log("\nTESTING: ONLY ARG1");
+argTest('first');
+console.log("\nTESTING: ONLY ARG1 + ARG2");
+argTest('first','second');
+console.log("\nTESTING ALL ARGS");
+argTest('first','second','third');
+console.log("\nTESTING: Undefined arg2");
+argTest("first", undefined, "third");
+console.log("\nTESTING: Too many arguments");
+argTest("first", "second", "third", "fourth");
+console.log("\nTESTING: Too many arguments with undefined");
+argTest(undefined, "second", undefined, "fourth");
