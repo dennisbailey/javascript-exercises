0c85ddf67ddef483d9d8507b536e1fcc9664a1b6
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-13 11:20:17 -0700

    Added comments

--- Practice.js
+++ Practice.js
@@ -1,30 +1,39 @@
 // Practice, practice, practice
 // Write a function that takes a string and returns the longest word in the string
 
-// Decalre the function longestWord that accepts one input
+// Declare the function longestWord that accepts one input
 function longestWord (input) {
-  var wordArray = input.split(" "); //an array to hold the split words
-//  console.log(wordArray);
+  // declare a new array wordArray and populate it the input split at the spaces
+  var wordArray = input.split(" ");
   
-  // An array to hold the length of words
-  var wordLengthArray = []; // An array to hold word lengths
-  for (i = 0; i < wordArray.length; i++) {  
+  // Declare an array called wordLengthArray that holds the lengths of each word
+  var wordLengthArray = [];
+  // Create a loop that loops through the values in wordArray
+  for (i = 0; i < wordArray.length; i++) {
+    // Declare a variable wordLength and set it to the lenght of each value in wordArray  
     var wordLength = wordArray[i].length
+    // Push value of wordLength to the wordLengthArray
     wordLengthArray.push(wordLength);
-//    console.log(wordLengthArray);
   }
   
-  //Find the greatest value in the word length array
+  // Find the greatest value in the word length array
+  // Declare longestWordPosiiton and set a default value of zero
   var longestWordPosition = 0;
-  var longestWordValue = 0;
+  // Declare longestWordLength and set a default value of zero
+  var longestWordLength = 0;
+  // Loop through each value of wordLengthArray
   for (i = 0; i < wordLengthArray.length; i++) {
-    if (wordLengthArray[i] > longestWordValue) {
-      longestWordValue = wordLengthArray[i];
+    // Compare the word length in the wordLengthArray with the sotred value of the longest word in the array
+    if (wordLengthArray[i] > longestWordLength) {
+      // If the current iteration of the loop is > than the longest word length 
+      // set longestWordLength to the new value from wordLengthArray
+      longestWordLength = wordLengthArray[i];
+      // Store the position of the longest word based on the current iteration
       longestWordPosition = i;
     }
   }
   
-  //Return the longest word
+  // Print to the console the longet word by referencing the wordArray using the position found in the variable longestWordPosition
   console.log(wordArray[longestWordPosition]); 
 }
 
