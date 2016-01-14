7affae06ab2bbcfd4df60f3645ae0483d692191b
Dennis Bailey <bailey.dennis@gmail.com>
2016-01-14 10:58:15 -0700

    Finished my version of the exercises

--- javascritpt-warmup-3.js
+++ javascritpt-warmup-3.js
@@ -38,6 +38,20 @@ function sumDigits (input) {
 
 sumDigits(generateRandomNum());
 
+// Generate a random number from 1000 to 9999 and output the sum of its digits, repeatedly, dropping the left-most digit after each iteration, until there is only one digit only.
+//For example, if the number is 1049, then the output should be 14, 13, 13, 9.
+
+// Create a function generateRandomNum to generate a random number from 1000 to 9999
+function generateRandomNum() {
+  // Declare the variable randomNum and set it equal to a random number * 10,000
+  // Round the random number up to the nearest integar
+  var randomNum = Math.ceil(Math.random() * 10000);
+  // Check for a random generation of zero
+  if (randomNum === 0) {randomNum = 1000;}
+  console.log(randomNum);
+  return randomNum;
+}
+
 // Declare a function sumArray with a single input parameter
 function sumArray (input) {
   arrDigitsToSum = input;
@@ -52,33 +66,17 @@ function sumDigitsFromString (input) {
 // Decalare digitToString
 // set it equal to the input parameter converted to a string
   var digitToString = input.toString();
-  console.log(digitToString)
-// Declare an empty array arrDigits
-  var arrDigits = [];
-// Loop through the digitToString and push each character to arrDigits
-  for (i = 0; i < digitToString.length; i++) {
-    var pushVal = digitToString.charAt(i);
-    console.log(pushVal)
-    arrDigits.push(pushVal);
+// Declare an array arrDigits
+// Use the split method on the string digitToString to populate this array
+  var arrDigits = digitToString.split('');
+  for ( j = 0; j < 4; j++) {
+    var arrToSum = arrDigits.slice(j, 4);
+    var result = sumArray (arrToSum);
+    console.log(result);
   }
-  console.log(arrDigits);
-} 
-  
-  do {
-// Call the sumArray function to convert to integers 
-// and sum the contents of arrDigits
-// Print the sum to the log
-  console.log(sumArray(arrDigits));
-// Remove the first value from arrDigits
-  arrDigits = arrDigits.shift();
-  }
-  while (arrDigits.length > 0);
 }
 
 sumDigitsFromString (1234);
-
-// Generate a random number from 1000 to 9999 and output the sum of its digits, repeatedly, dropping the left-most digit after each iteration, until there is only one digit only.
-//For example, if the number is 1049, then the output should be 14, 13, 13, 9.
-
+sumDigitsFromString (generateRandomNum());
 
 // Reverse engineer with a group member
